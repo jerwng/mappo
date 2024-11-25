440d2401fb2d654fd077c511e78bbae951388255
Jerry Wang <chenyuan.wang01@utrgv.edu>
2024-11-24 21:47:22 -0600

    Increase the timestep to 20000000

--- onpolicy/scripts/train_mpe_scripts/train_mpe_tag_mat.sh
+++ onpolicy/scripts/train_mpe_scripts/train_mpe_tag_mat.sh
@@ -13,6 +13,6 @@ do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python /home/chenyuanwang01/mappo/onpolicy/scripts/train/train_mpe.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
-    --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 200000 \
+    --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 20000000 \
     --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --use_wandb
 done
